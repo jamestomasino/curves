0f140c1e6581851bafebb982f50d59186a7c432d
James Tomasino <james@tomasino.org>
2013-12-26 16:02:51 -0500

    added highchart for graphing

--- sjs/libs.js
+++ sjs/libs.js
@@ -1,2 +1,2 @@
-//= require ./lib/jquery-1.10.1.min
-//= require lib/jquery.numeric
+//= require lib/jquery-1.10.1.min
+//= require lib/highcharts-all.js
--- sjs/main.js
+++ sjs/main.js
@@ -1,108 +1,274 @@
-var scores = [ 90, 80, 70, 75, 88, 91, 75, 55, 59, 69, 60, 50, 40 ];
-var sort_scores = [];
-var mean_squares = [];
-var mean_fourths = [];
-var log_scores = [];
-var z_scores = [];
+$(function () {
 
-// Median:
-// Middle value when sorted
-var median = 0;
-var mid = 0;
-sort_scores = scores.slice(0).sort();
+	var scores = [ 90, 80, 70, 75, 88, 91, 75, 55, 59, 69, 60, 50, 75, 88, 91, 75, 55, 59, 69, 60, 50, 55, 59, 69, 60, 50, 40 ];
+	var sort_scores = [];
+	var mean_squares = [];
+	var mean_fourths = [];
+	var log_scores = [];
+	var z_scores = [];
 
-if ( sort_scores.length % 2 === 0 ) {
-	mid = sort_scores.length / 2;
-	median = sort_scores[mid];
-} else {
-	mid = Math.floor(sort_scores.length / 2);
-	median = (sort_scores[mid] + sort_scores[mid+1] ) / 2
-}
+	// Median:
+	// Middle value when sorted
+	var median = 0;
+	var mid = 0;
+	sort_scores = scores.slice(0).sort(compareNumbers);
 
-// Mean:
-// Sum all scores and divide by n
-var mean = sum(scores) / scores.length;
+	function compareNumbers(a, b) {
+		return a - b;
+	}
 
-// Variance:
-// For each score: (x - μ)^2
-// Calculate μ of result set
-mean_squares = mean_power(scores, mean, 2);
-variance = sum(mean_squares) / mean_squares.length;
+	if ( sort_scores.length % 2 === 0 ) {
+		mid = sort_scores.length / 2;
+		median = sort_scores[mid];
+	} else {
+		mid = Math.floor(sort_scores.length / 2);
+		median = (sort_scores[mid] + sort_scores[mid+1] ) / 2
+	}
 
-// Standard Deviation:
-// Square Root of Variance
-standard_deviation = Math.sqrt(variance);
+	// Mean:
+	// Sum all scores and divide by n
+	var mean = sum(scores) / scores.length;
 
-// Skew:
-// 3(Mean - Median)
-var skew = 3 * ( mean - median );
+	// Variance:
+	// For each score: (x - μ)^2
+	// Calculate μ of result set
+	mean_squares = mean_power(scores, mean, 2);
+	variance = sum(mean_squares) / mean_squares.length;
 
-// Kurtosis:
-// (x - μ)^4 / standard_deviation^4
-mean_fourths = mean_power(scores, mean, 4);
-var kurtosis = sum(mean_fourths) / (mean_fourths.length * Math.pow( standard_deviation, 4 ) );
+	// Standard Deviation:
+	// Square Root of Variance
+	standard_deviation = Math.sqrt(variance);
 
-// SEK (Standard error of kurtosis):
-// sek = 2(SES) Math.sqrt( (n^2-1) / (n-3)(n+5) )
-var n = scores.length;
-sek = 2 * Math.sqrt ( ((n*n)-1) / ( (n-3) * (n + 5) ) );
+	// Skew:
+	// 3(Mean - Median)
+	var skew = 3 * ( mean - median );
 
-// Z-Score:
-// ( x - μ ) / standard_deviation
-z_scores = z_score ( scores, mean, standard_deviation );
+	// Kurtosis:
+	// (x - μ)^4 / standard_deviation^4
+	mean_fourths = mean_power(scores, mean, 4);
+	var kurtosis = sum(mean_fourths) / (mean_fourths.length * Math.pow( standard_deviation, 4 ) );
+
+	// SEK (Standard error of kurtosis):
+	// sek = 2(SES) Math.sqrt( (n^2-1) / (n-3)(n+5) )
+	var n = scores.length;
+	sek = 2 * Math.sqrt ( ((n*n)-1) / ( (n-3) * (n + 5) ) );
+
+	// Z-Score:
+	// ( x - μ ) / standard_deviation
+	z_scores = z_score ( scores, mean, standard_deviation );
 
 
-log_scores = log_score( scores );
-var log_sort_scores = log_scores.slice(0).sort();
-var log_mean = sum ( log_scores ) / log_scores.length;
-var log_mean_squares = mean_power ( log_scores, log_mean, 2 );
-var log_variance = sum(log_mean_squares) / log_mean_squares.length;
-var log_standard_deviation = Math.sqrt ( log_variance );
-var log_mean_fourths = mean_power(log_scores, log_mean, 4);
-var log_kurtosis = sum(log_mean_fourths) / (log_mean_fourths.length * Math.pow( log_standard_deviation, 4 ) );
-var log_mid, log_median;
-if ( log_sort_scores.length % 2 === 0 ) {
-	log_mid = log_sort_scores.length / 2;
-	log_median = log_sort_scores[log_mid];
-} else {
-	log_mid = Math.floor(log_sort_scores.length / 2);
-	log_median = (log_sort_scores[log_mid] + log_sort_scores[log_mid+1] ) / 2
-}
-var log_skew = 3 * ( log_mean - log_median );
-var log_z_scores = z_score ( log_scores, log_mean, log_standard_deviation );
+	log_scores = log_score( scores );
+	var log_sort_scores = log_scores.slice(0).sort();
+	var log_mean = sum ( log_scores ) / log_scores.length;
+	var log_mean_squares = mean_power ( log_scores, log_mean, 2 );
+	var log_variance = sum(log_mean_squares) / log_mean_squares.length;
+	var log_standard_deviation = Math.sqrt ( log_variance );
+	var log_mean_fourths = mean_power(log_scores, log_mean, 4);
+	var log_kurtosis = sum(log_mean_fourths) / (log_mean_fourths.length * Math.pow( log_standard_deviation, 4 ) );
+	var log_mid, log_median;
+	if ( log_sort_scores.length % 2 === 0 ) {
+		log_mid = log_sort_scores.length / 2;
+		log_median = log_sort_scores[log_mid];
+	} else {
+		log_mid = Math.floor(log_sort_scores.length / 2);
+		log_median = (log_sort_scores[log_mid] + log_sort_scores[log_mid+1] ) / 2
+	}
+	var log_skew = 3 * ( log_mean - log_median );
+	var log_z_scores = z_score ( log_scores, log_mean, log_standard_deviation );
 
-//console.log ('scores:', scores);
-console.log ('mean:', mean);
-console.log ('median:', median);
-console.log ('variance:', variance);
-console.log ('standard-deviation:', standard_deviation );
-console.log ('skew', skew);
-console.log ('kurtosis:', kurtosis);
-console.log ('sek:', sek);
-//console.log ('z-scores:', z_scores);
+	//console.log ('scores:', scores);
+	//console.log ('mean:', mean);
+	//console.log ('median:', median);
+	//console.log ('variance:', variance);
+	//console.log ('standard-deviation:', standard_deviation );
+	//console.log ('skew', skew);
+	//console.log ('kurtosis:', kurtosis);
+	//console.log ('sek:', sek);
+	//console.log ('z-scores:', z_scores);
 
-console.log ('----------------------------');
+	//console.log ('----------------------------');
 
-//console.log ('log-scores:', log_scores);
-console.log ('log-mean:', log_mean);
-console.log ('log-median:', log_median);
-console.log ('log-variance:', log_variance);
-console.log ('log-skew', log_skew);
-console.log ('log-standard-deviation:', log_standard_deviation);
-console.log ('log-kurtosis:', log_kurtosis);
-//console.log ('log-z-scores:', log_z_scores);
+	//console.log ('log-scores:', log_scores);
+	//console.log ('log-mean:', log_mean);
+	//console.log ('log-median:', log_median);
+	//console.log ('log-variance:', log_variance);
+	//console.log ('log-skew', log_skew);
+	//console.log ('log-standard-deviation:', log_standard_deviation);
+	//console.log ('log-kurtosis:', log_kurtosis);
+	//console.log ('log-z-scores:', log_z_scores);
 
-console.log ('----------------------------');
+	//console.log ('----------------------------');
 
-var grades_perc = get_grade_by_perc ( scores );
-var grades_raw = get_letter_grades ( z_scores );
-var grades_log = get_letter_grades ( log_z_scores );
+	var grades_perc = get_grade_by_perc ( scores );
+	var grades_raw = get_letter_grades ( z_scores );
+	var grades_log = get_letter_grades ( log_z_scores );
 
-console.log ('scores:', scores);
-console.log ( 'Perc Grades:', grades_perc );
-console.log ( 'Raw Grades:', grades_raw );
-console.log ( 'Log Grades:', grades_log );
+	console.log ('scores:', scores);
+	console.log ( 'Per Grades:', grades_perc );
+	console.log ( 'Raw Grades:', grades_raw );
+	console.log ( 'Log Grades:', grades_log );
 
+
+	$('#scores').highcharts({
+		chart: {
+			zoomType: 'xy'
+		},
+		title: {
+			text: 'Grading Curve'
+		},
+		subtitle: {
+		},
+		xAxis: [{
+			labels: { enabled: false }
+		}],
+		yAxis: [{ // Primary yAxis
+			max: 2,
+			min: -3,
+			labels: {
+				format: '{value}',
+				style: {
+					color: '#89A54E'
+				}
+			},
+			title: {
+				text: 'Z Score',
+				style: {
+					color: '#89A54E'
+				}
+			}
+		}, { // Secondary yAxis
+			max: 100,
+			min: 0,
+			title: {
+				text: 'Percentage',
+				style: {
+					color: '#4572A7'
+				}
+			},
+			labels: {
+				format: '{value}%',
+				style: {
+					color: '#4572A7'
+				}
+			},
+			opposite: true
+		}],
+		tooltip: {
+			shared: true
+		},
+		legend: {
+			layout: 'vertical',
+			align: 'left',
+			x: 50,
+			verticalAlign: 'top',
+			y: 320,
+			floating: true,
+			backgroundColor: '#FFFFFF'
+		},
+		series: [{
+			name: 'Score',
+			color: '#4572A7',
+			type: 'scatter',
+			yAxis: 1,
+			data: scores,
+			tooltip: {
+				valueSuffix: '%'
+			}
+
+		}, {
+			name: 'Z Score',
+			color: '#89A54E',
+			type: 'spline',
+			data: z_scores,
+			tooltip: {
+				valueSuffix: ''
+			}
+		}, {
+			name: 'Transformed Z Score',
+			color: '#E10000',
+			type: 'spline',
+			data: log_z_scores,
+			tooltip: {
+				valueSuffix: ''
+			}
+
+		}]
+	});
+});
+
+	//$('#scores').highcharts({
+		//chart: {
+			//zoomType: 'xy'
+		//},
+		//title: {
+			//text: 'Grades',
+		//},
+		//subtitle: {
+		//},
+		//xAxis: {
+		//},
+		//yAxis: [{ // Primary yAxis
+			//labels: {
+				//format: '{value}%',
+				//style: {
+					//color: '#89A54E'
+				//}
+			//},
+			//title: {
+				//text: 'Score',
+				//style: {
+					//color: '#89A54E'
+				//}
+			//}
+		//}, { // Secondary yAxis
+			//title: {
+				//text: 'Z-Score',
+				//style: {
+					//color: '#4572A7'
+				//}
+			//},
+			//labels: {
+				//format: '{value}',
+				//style: {
+					//color: '#4572A7'
+				//}
+			//},
+			//opposite: true
+		//}],
+		//yAxis: {
+			//title: {
+				//text: 'Score'
+			//},
+			//plotLines: [{
+				//value: 0,
+				//width: 1,
+				//color: '#808080'
+			//}]
+		//},
+		//tooltip: {
+			//valueSuffix: '%',
+			//shared: true
+		//},
+		//series: [{
+			//name: 'Score',
+			//color: '#4572A7',
+			//type: 'column',
+			//yAxis: 1,
+			//data: scores,
+			//tooltip: {
+				//valueSuffix: '%'
+			//}
+		//}, {
+			//name: 'Z Score',
+			//color: '#89A54E',
+			//type: 'spline',
+			//data: scores,
+			//tooltip: {
+				//valueSuffix: ''
+			//}
+		//}]
+	//});
 
 function sum ( arr ) {
 	var s = 0;
@@ -140,7 +306,7 @@ function get_grade_by_perc (arr) {
 	var grades = [];
 	var i = arr.length; while (i--) {
 		var grade = "F";
-		if ( arr[i] >= 95 ) {
+		if ( arr[i] >= 97 ) {
 			grade = "A+";
 		} else if ( arr[i] >= 90 ) {
 			grade = "A";
@@ -162,7 +328,7 @@ function get_letter_grades ( arr ) {
 	var grades = [];
 	var i = arr.length; while (i--) {
 		var grade = "F";
-		if ( arr[i] >= 2 ) {
+		if ( arr[i] >= 1.7 ) {
 			grade = "A+";
 		} else if ( arr[i] >= 1 ) {
 			grade = "A";
